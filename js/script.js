31b71a30c0a92ad788b27875a3635e6e32dd3ef3
folke ishii <folke.steen85@gmail.com>
2020-02-05 20:27:38 +0100

    fixed navbar some more

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@ const dmSlider = document.querySelector('.darkMode')
 const dmSkyBlue = document.querySelectorAll('.skyblue');
 const dmSub = document.querySelectorAll('.subdm');
 
+window.onscroll = function() {navScroll()};
+
+function navScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    console.log(winScroll);
+    if (winScroll > 50) {
+        document.querySelector(".menu").classList.add("scrollD")
+        document.querySelector(".mainMenu").classList.add("scrollD")
+    } else {
+        document.querySelector(".menu").classList.remove("scrollD")
+        document.querySelector(".mainMenu").classList.remove("scrollD")
+    }
+}
+
 function toggleBurgerMenu () {
     document.querySelector('.mainMenu').classList.toggle('navActive');
     burger.classList.toggle('toggle');
@@ -19,6 +34,15 @@ function toggleNightMode() {
     document.querySelector('.mainPart').classList.toggle('mainPartdm');
     document.querySelector('body').classList.toggle('mainPartdm');
 }
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
 
 burger.addEventListener('click', toggleBurgerMenu)
 dmSlider.addEventListener('change', toggleNightMode)
