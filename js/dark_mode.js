b9568d151b8a010db2342354173988fc6a961753
folke ishii <folke.steen85@gmail.com>
2020-03-22 23:01:23 +0100

    Fixed blog, final push. Also I forgot to add my sketch to this project

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,7 +1,17 @@
 const whiteText = document.querySelectorAll('.text-white');
 const color3 = document.querySelectorAll('.color-3');
+
+const mainPart = document.querySelector(".mainPart");
+
 const blog = document.querySelector('#blog');
-const blue_nav_bar = document.querySelector('.blue_nav_bar')
+const about = document.querySelector("#about");
+
+const nav_white_background = document.querySelector(".nav_white_background");
+const white_b = document.querySelector(".white_b");
+
+const blog_site = document.querySelector(".my_blog");
+const about_site = document.querySelector(".main_about_me");
+const my_projects = document.querySelector(".my_projects");
 
 function doBlackText() {
     var i;
@@ -29,8 +39,18 @@ function unique_selectors_dark_mode() {
     if (blog) {
         blog.classList.add("blogdm")
     }
-    if (blue_nav_bar) {
-        blue_nav_bar.classList.add("blue_nav_bar_dm");
+    if (about) {
+        about.classList.add("aboutdm")
+    }
+    if (mainPart) {
+        mainPart.classList.add("mainPartdm")
+    }
+    if (nav_white_background) {
+        white_b.classList.add("white_b_dm")
+        nav_white_background.classList.add("nav_background_dm")
+    }
+    if (blog_site) {
+        blog_site.classList.add("my_blog_dm")
     }
 }
 
@@ -38,17 +58,25 @@ function unique_selectors_light_mode() {
     if (blog) {
         blog.classList.remove("blogdm")
     }
-    if (blue_nav_bar) {
-        blue_nav_bar.classList.remove("blue_nav_bar_dm");
+    if (about) {
+        about.classList.remove("aboutdm")
+    }
+    if (mainPart) {
+        mainPart.classList.remove("mainPartdm")
+    }
+    if (nav_white_background) {
+        white_b.classList.remove("white_b_dm")
+        nav_white_background.classList.remove("nav_background_dm")
+    }
+    if (blog_site) {
+        blog_site.classList.remove("my_blog_dm")
     }
 }
 
 function toggleNightMode() {
     document.getElementById("darkModeCheck").checked ? 
     (
-        document.querySelector(".mainPart").classList.add("mainPartdm"), 
         document.querySelector("body").classList.add("mainPartdm"),
-        document.querySelector("#about").classList.add("aboutdm"),
         document.querySelector(".footerCopyright").classList.add("copyrightdm"),
         document.querySelector(".footer").classList.add("footerdm"),
         doWhiteText(),
@@ -56,9 +84,7 @@ function toggleNightMode() {
     ) 
     : 
     (
-        document.querySelector(".mainPart").classList.remove("mainPartdm"), 
         document.querySelector("body").classList.remove("mainPartdm"),
-        document.querySelector("#about").classList.remove("aboutdm"),
         document.querySelector(".footerCopyright").classList.remove("copyrightdm"),
         document.querySelector(".footer").classList.remove("footerdm"),
         doBlackText(),
